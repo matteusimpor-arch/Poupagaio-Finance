bbfdacef6bffde7c047672c6eeebe9a34f502fd6
Mateus Araujo <matteus.impor@gmail.com>
2026-09-22 20:30:31 -0300

    Update mascot.ts

--- src/assets/mascot.ts
+++ src/assets/mascot.ts
@@ -1,2 +1,2 @@
-export const POUPAGAIO_MASCOT_URL = '/branding/poupagaio-logo.png';
-export const POUPAGAIO_ASSISTANTE_URL = '/branding/poupagaio-logo.png';
+export const POUPAGAIO_MASCOT_URL = '/branding/poupagaio-logooficial.png';
+export const POUPAGAIO_ASSISTANTE_URL = '/branding/poupagaio-logooficial.png';
